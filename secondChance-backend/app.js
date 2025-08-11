003c0ca1a37830822fb6817ba2df4913bd90a9e3
ntpon <nattapon.suetrong@gmail.com>
2025-08-12 02:00:52 +0700

    Refactor code to ci/cd

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,11 +1,10 @@
-/*jshint esversion: 8 */
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pinoLogger = require("./logger");
+const path = require("path");
 
 const connectToDatabase = require("./models/db");
-const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
 app.use("*", cors());
@@ -21,33 +20,22 @@ connectToDatabase()
 app.use(express.json());
 
 // Route files
-
-// authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
+const secondChanceRoutes = require("./routes/secondChanceItemsRoutes");
 const authRoutes = require("./routes/authRoutes");
-
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-const secondChanceItemsRoutes = require("./routes/secondChanceItemsRoutes");
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require("./routes/searchRoutes");
-
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
 
 app.use(pinoHttp({ logger }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Use Routes
-// authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
+app.use("/api/secondchance/items", secondChanceRoutes);
 app.use("/api/auth", authRoutes);
-
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-app.use("/api/secondchance/items", secondChanceItemsRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use("/api/secondchance/search", searchRoutes);
 
 // Global Error Handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err);
   res.status(500).send("Internal Server Error");
 });
